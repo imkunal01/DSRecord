4a9d6733e646b4b245351020a85b6d2c039033d2
imkunal01 <kunaldhangar184@gmail.com>
2025-12-18 12:47:27 +0530

    asdasd

--- TleEliiminators/LineTrip.cpp
+++ TleEliiminators/LineTrip.cpp
@@ -1,29 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
 
-        int n,k;
-        cin>>n;
-        cin>>k;
-        vector<long long> a;
-        a.push_back(0);
-        for(int i=0;i<n;i++){
-            long long p;
-            cin>>p;
-            a.push_back(p);
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        int ans = arr[0];
+        for(int i = 1;i<n;i++){
+          ans = max(ans,arr[i]-arr[i-1]);
         }
-
-        a.push_back(k);
-        n=a.size();
-        long long res = INT_MIN;
-        for(int i =1;i<n;i++){
-            if(i==n-1) res = max(res , 2*(a[i] -a[i-1]));
-            else res = max(res , a[i] -a[i-1]);
-        }
-        cout<<res<<endl;
+        ans = max(ans,2*(x- arr[n-1]));
+        cout<<ans<<endl;
+        
     }
 }
+
+
+
+// 3
+// 3 7
+// 1 2 5
+// 3 6
+// 1 2 5
+// 1 10
+// 7
