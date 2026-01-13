d38115e8219ec57c355bae6290474884bb0585ee
kunal <kunaldhangar184@gmail.com>
2026-01-13 21:41:57 +0530

    Implement two-sum algorithm in tempCodeRunnerFile.cpp and update input handling

--- LeetCode/tempCodeRunnerFile.cpp
+++ LeetCode/tempCodeRunnerFile.cpp
@@ -1,20 +1,45 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<algorithm>
-#include<cctype>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
-    vector<string> arr = {"kunal", "rocks", "code"};
-    for(string &s : arr){
-        for(char &ch: s){
-            ch = static_cast<char>(toupper(static_cast<unsigned char>(ch)));
+int main() {
+    vector<int> v;
+    // Enter target
+    int t, s;
+    cin >> t;
+    // Enter size
+    cin >> s;
+    for (int i = 0; i < s; i++) {
+        int inp;
+        cin >> inp;
+         v.push_back(inp);
+    }
+
+    for (int i = 0; i < s; i++) {
+        for (int j = i + 1; j < s; j++) {
+            if (v[i] + v[j] == t) {
+                cout << i << "," << j << endl;
+            }
         }
     }
-    for(const string &i : arr){
-        cout<<i<<" ";
-    }
-    cout<<endl;
+         // class Solution {
+         // public:
+         //    vector<int> twoSum(vector<int>& nums, int target) {
+         //       vector<int>v;
+         //       int n = nums.size();
+               
+         //       for (int i = 0; i < n; i++) {
+         //             for (int j = i + 1; j < n; j++) {
+         //                if (nums[i] + nums[j] == target) {
+         //                   v.push_back(i);
+         //                   v.push_back(j);
+         //                   return v;  
+         //                }
+         //             }
+         //       }
+               
+         //       return v;
+         //    }
+         // };
     return 0;
 }
