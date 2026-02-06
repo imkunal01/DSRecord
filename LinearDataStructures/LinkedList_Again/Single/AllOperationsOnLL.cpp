cad9b5bc90e7b24187d6bb7eedfe15dd25f17a53
kunal <kunaldhangar184@gmail.com>
2026-02-06 11:49:14 +0530

    Refactor code structure for improved readability and maintainability

--- LinearDataStructures/LinkedList_Again/Single/AllOperationsOnLL.cpp
+++ LinearDataStructures/LinkedList_Again/Single/AllOperationsOnLL.cpp
@@ -28,6 +28,18 @@ Node* ConvetArrToLL(vector<int>arr){
     }
     return head;
 }
+
+// convert array to linked list
+vector<int>convertLLToArr(Node* head){
+    vector<int>arr;
+    Node* temp = head;
+    while(temp != NULL){
+        arr.push_back(temp->data);
+        temp = temp->next;
+    }
+    return arr;
+}
+
 Node* deleteTail(Node* head){
     if(head == NULL || head->next == NULL){
         delete head;
@@ -42,6 +54,7 @@ Node* deleteTail(Node* head){
     temp->next = NULL;
     return head;
 }
+
 Node* deleteHead(Node* head){
     if(head == NULL){
         return head;
@@ -51,6 +64,7 @@ Node* deleteHead(Node* head){
     delete temp;
     return head;
 }
+
 void display(Node* head){
     Node* temp = head;
     while(temp != NULL){
@@ -59,6 +73,7 @@ void display(Node* head){
     }
     cout<<endl;
 }
+
 Node* deleteAtPos(Node* head,int k){
     if(k == 1){
         Node* temp = head;
@@ -78,44 +93,99 @@ Node* deleteAtPos(Node* head,int k){
         temp = temp->next;
     }
     return head;
-      
 
 }
-Node* deleteMatchingValue(Node* head,int val){
-    if(val == head->data){
+
+Node* deleteMatchingValue(Node* head, int val) {
+    if (head == NULL) return head;
+    
+    while(head!= NULL && head->data == val){
         Node* temp = head;
         head = head->next;
         delete temp;
-        return head;
     }
-    if(val == 1){
-        Node *temp = head; 
-        delete temp;
-        return head;
+    if (head == NULL) return head;
 
-    }
-    Node *temp = head; 
-    Node*prev = NULL;
-    while(temp!=NULL){
-        if(temp->data == val){
-            prev->next = prev->next->next;
+    Node* curr = head;
+    while (curr->next != NULL) {
+        if (curr->next->data == val) {
+            Node* temp = curr->next;
+            curr->next = curr->next->next;
             delete temp;
-            break;
         }
-        prev = temp;
-        temp = temp->next;
+        else{
+            curr = curr->next;
+        }
     }
-    return head;
-      
 
+    return head;
 }
+
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int target) {
+        if(target == head->val){
+            ListNode* temp = head;
+            head = head->next;
+            return head;
+        }
+        if(target == 1){
+            ListNode* temp = head;
+            delete temp;
+            return head;
+        }
+
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+
+        while(temp != NULL){
+            if(temp->val == target){
+                prev->next = prev->next->next;
+                delete temp;
+                break;
+            }
+            prev = temp;
+            temp = temp->next;
+        }
+        return head;
+    }
+};
+ListNode* removeElements(ListNode* head, int val) {
+        ListNode* temp = new ListNode(0);
+        temp->next = head;
+        ListNode* curr = temp;
+        while (curr->next != NULL) {
+            if (curr->next->val == val)
+                curr->next = curr->next->next;
+            else
+                curr = curr->next;
+        }
+        return temp->next;
+    }
+    
 Node* insertAtHead(Node* head,int val){
     Node* temp = new Node(val,head);
     return temp;
+}
+Node* insertSorted(Node* head, int val){
+    Node* newNode = new Node(val);
+    newNode->data = val;
+    head = newNode;
 
 }
+
 int main() {
-    vector<int>arr = {1,2,3,4,5};
+    vector<int>arr = {1,2,3,7,7,7};
     Node* head = ConvetArrToLL(arr);
     cout<<"Array Converted To LinkedList"<<endl;
     display(head);
@@ -128,7 +198,9 @@ int main() {
     // cout<<"After Deletion at k=2"<<endl;
     // deleteAtPos(head,3);
     // head = deleteMatchingValue(head,1);
+    int target = 7;
     head = insertAtHead(head,100);
+    deleteMatchingValue(head,target);
     display(head);
     return 0;
 }
---
+++ LinearDataStructures/LinkedList_Again/Single/tempCodeRunnerFile.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+    Node(int data){ this->data = data; this->next = NULL; }
+    Node(Node* next,int data){ this->data = data; this->next = NULL; }
+    Node(int data, Node* next) : data(data), next(next) {}
+};
+
+
+bool CheckPalindrom(Node* head){
+    Node* curr = head;
+    stack<int> s;
+    while(curr != NULL){
+        s.push(curr->data);
+        curr = curr->next;
+
+    }
+    curr = head;
+    while(curr){
+        if(curr->data != s.top()) return false;
+        s.pop();
+        curr= curr->next;
+    }
+    return true;
+}
+
+int main() {
+    // Test: binary 1->0->1->1 (11)
+    Node* n4 = new Node(1);
+    Node* n3 = new Node(2, n4);
+    Node* n2 = new Node(2, n3);
+    Node* n1 = new Node(2, n2);
+
+    int result = CheckPalindrom(n4);
+    if (result != 1) {
+        cout << "Test failed: expected 11, got " << result << "\n";
+        return 1;
+    }
+
+    cout << result<<" All tests passed\n";
+
+    delete n1;
+    delete n2;
+    delete n3;
+    delete n4;
+    return 0;
+}
